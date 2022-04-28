dc6ea102310682ebd8f730946c0a967e3b1de03d
lekyvong09 <lekyvong09@gmail.com>
2022-04-28 19:59:36 +0700

    2022Apr28- apply generic repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -14,34 +14,38 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private IProductRepository _productRepository;
+        private IGenericRepository<Product> _productRepository;
+        private IGenericRepository<ProductBrand> _productBrandRepository;
+        private IGenericRepository<ProductType> _productTypeRepository;
 
-        public ProductsController(IProductRepository productRepository)
+        public ProductsController(IGenericRepository<Product> productRepository, IGenericRepository<ProductBrand> productBrandRepository, IGenericRepository<ProductType> productTypeRepository)
         {
             _productRepository = productRepository;
+            _productBrandRepository = productBrandRepository;
+            _productTypeRepository = productTypeRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts() {
-            List<Product> products = await _productRepository.GetProductsAsync();
+            List<Product> products = await _productRepository.GetAllAsync();
             return Ok(products);
         }
 
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id) {
-            Product product = await _productRepository.GetProductByIdAsync(id);
+            Product product = await _productRepository.GetByIdAsync(id);
             return Ok(product);
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<List<ProductBrand>>> GetProductBrands() {
-            return await _productRepository.GetProductBrandsAsync();
+            return await _productBrandRepository.GetAllAsync();
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<List<ProductType>>> GetProductTypes() {
-            return await _productRepository.GetProductTypesAsync();
+            return await _productTypeRepository.GetAllAsync();
         }
         
     }
