0d68956c426396dd808b060541c113a5d5b3d4c0
lekyvong09 <lekyvong09@gmail.com>
2022-05-05 20:24:35 +0700

    AutoMapper 3 - resolver with environment variable

--- API/Helpers/ProductImageUrlResolver.cs
+++ API/Helpers/ProductImageUrlResolver.cs
@@ -7,13 +7,23 @@ namespace API.Helpers
 {
 	public class ProductImageUrlResolver : IValueResolver<Product, ReturnProduct, string>
 	{
-		public ProductImageUrlResolver()
-		{
-		}
+        /// IConfiguration is used to get environment variables
+        private IConfiguration _config;
+
+        public ProductImageUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
 
         public string Resolve(Product source, ReturnProduct destination, string destMember, ResolutionContext context)
         {
-            return "https://localhost:5001/" + source.PictureUrl;
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                string baseUrl = _config["ApiUrl"];
+
+                return baseUrl + source.PictureUrl;
+            }
+            return null;
         }
     }
 }
