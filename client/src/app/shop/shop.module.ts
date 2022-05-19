9e33baaf985fd0e6fbd4a44c720123d41cdb2533
lekyvong09 <lekyvong09@gmail.com>
2022-05-19 21:08:37 +0700

    Ng - generate product-item

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule,
