7152018850c78aef2594178fb2884a051a8d605b
lekyvong09 <lekyvong09@gmail.com>
2022-05-19 21:05:58 +0700

    Ng - product layout

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../_models/product';
 import { ShopService } from './shop.service';
 
+import {faSearch, faRefresh} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  /// fontawesome icon
+  faSearch = faSearch; faRefresh = faRefresh;
+
   products: IProduct[] = [];
   constructor(private shopService: ShopService) { }
 
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -9,7 +10,8 @@ import { ShopComponent } from './shop.component';
     ShopComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
   ],
   exports: [
     ShopComponent,
