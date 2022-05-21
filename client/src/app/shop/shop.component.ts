95f0f6563e7dd5707d8960c42dbcf7405b0e4384
lekyvong09 <lekyvong09@gmail.com>
2022-05-21 19:53:44 +0700

    Ng - add filter Product Type & Brand

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -3,6 +3,8 @@ import { IProduct } from '../_models/product';
 import { ShopService } from './shop.service';
 
 import {faSearch, faRefresh} from '@fortawesome/free-solid-svg-icons';
+import { IBrand } from '../_models/brand';
+import { IType } from '../_models/product-type';
 
 @Component({
   selector: 'app-shop',
@@ -14,9 +16,18 @@ export class ShopComponent implements OnInit {
   faSearch = faSearch; faRefresh = faRefresh;
 
   products: IProduct[] = [];
+  brands: IBrand[] = [];
+  productTypes: IType[] = [];
+
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(): void {
     this.shopService.getProducts().subscribe(response => {
       console.log(response);
       this.products = response.data;
@@ -25,4 +36,15 @@ export class ShopComponent implements OnInit {
     });
   }
 
+  getBrands(): void {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = response;
+    }, error => console.log(error));
+  }
+
+  getTypes(): void {
+    this.shopService.getTypes().subscribe(response => {
+      this.productTypes = response;
+    }, error => console.log(error));
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IBrand } from '../_models/brand';
 import { IPagination } from '../_models/pagination';
+import { IType } from '../_models/product-type';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,13 @@ export class ShopService {
 
   getProducts(): Observable<IPagination> {
     return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
+
+  getBrands(): Observable<IBrand[]> {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes(): Observable<IType[]> {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
