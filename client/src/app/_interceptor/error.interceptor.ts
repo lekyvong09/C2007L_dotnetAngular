d5c25658f6defde44b9aedaa356f8511adc3a69d
lekyvong09 <lekyvong09@gmail.com>
2022-05-31 20:05:54 +0700

    Ng - Handle error 400 validation & 500 server error

--- client/src/app/_interceptor/error.interceptor.ts
+++ client/src/app/_interceptor/error.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
@@ -19,13 +19,21 @@ export class ErrorInterceptor implements HttpInterceptor {
       catchError(error => {
         if (error) {
           if (error.status === 400) {
+            if (error.error.errors){
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+          if (error.status === 401) {
             this.toastr.error(error.error.message, error.error.statusCode);
           }
           if (error.status === 404) {
             this.router.navigateByUrl('/not-found');
           }
           if (error.status === 500) {
-            this.router.navigateByUrl('/server-error');
+            let navigationExtras: NavigationExtras = {state: {error: error.error}};
+            this.router.navigateByUrl('/server-error', navigationExtras);
           }
         }
         return throwError(error);
