2d4f719cc1fbfa5f9d9ba56072bee9f05b3bd46e
lekyvong09 <lekyvong09@gmail.com>
2022-06-04 20:12:37 +0700

    Ng - Initialize basket

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 import { IPagination } from './_models/pagination';
 import { IProduct } from './_models/product';
 
@@ -12,11 +13,18 @@ import { IProduct } from './_models/product';
 export class AppComponent implements OnInit {
   title = 'Book shop';
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
-  }
+    /// auto load basket_id whenever customer visit back our website
+    var basketId = localStorage.getItem('basket_id');
 
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('basket is initialized');
+      }, error => console.log(error));
+    }
+  }
 
 
 }
--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -24,6 +24,7 @@ export class BasketService {
       .pipe(
         map((basket: IBasket) => {
           this.basketSource.next(basket);
+          console.log(this.getCurrentBasketValue());
         })
       );
   }
